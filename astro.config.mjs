598fea9547146513d2c3e4321f7c4487fd8acb81
S-s-Spudd <77349475+S-s-Spudd@users.noreply.github.com>
2026-06-29 13:43:26 +0100

    fix: configure astro for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://s-s-spudd.github.io',
+  base: '/MattzBarbers-Sample',
   output: 'static',
   integrations: [react()],
   prefetch: false,
